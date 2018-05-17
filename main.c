42b87c197279bafa6affa27e58894c65329e8074
budd <17973512+githubcomusr@users.noreply.github.com>
2018-05-17 09:13:47 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,6 @@
 #include<stdio.h>
-
-int main(){
-int i = 0;
-
-if(i<100){
-printf("This is the %d time print. \n",i);
-
+int main(){int i = 0;
+while(i<100){printf("This is the %d time print. \n",i+1);
 i = i + 1;
+}return 0;}
 
-}else{
-return 0;
-}
-
-}
